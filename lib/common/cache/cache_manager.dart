97b596b522b42bb12f9f23b8ecec7efb36aeb59b
CoMatu <30466272+CoMatu@users.noreply.github.com>
2023-04-06 21:59:50 +0300

    add db in progress

--- lib/common/cache/cache_manager.dart
+++ lib/common/cache/cache_manager.dart
@@ -1,34 +1,43 @@
+import 'package:test_farma/common/database/hive_service.dart';
+
 abstract class CacheManager {
   Future<bool> writeUserRequestDataWithTime(
       String key, String model, Duration time);
   Future<String> getUserRequestDataOnString(String key);
   Future<bool> removeUserRequestSingleCache(String key);
   Future<bool> removeUserRequestCache(String key);
+  Future<bool> hasToken();
 }
 
 class CacheManagerImpl implements CacheManager {
   @override
-  Future<String> getUserRequestDataOnString(String key) {
+  Future<String> getUserRequestDataOnString(String key) async {
     // TODO: implement getUserRequestDataOnString
     throw UnimplementedError();
   }
 
   @override
-  Future<bool> removeUserRequestCache(String key) {
+  Future<bool> removeUserRequestCache(String key) async {
     // TODO: implement removeUserRequestCache
     throw UnimplementedError();
   }
 
   @override
-  Future<bool> removeUserRequestSingleCache(String key) {
+  Future<bool> removeUserRequestSingleCache(String key) async {
     // TODO: implement removeUserRequestSingleCache
     throw UnimplementedError();
   }
 
   @override
   Future<bool> writeUserRequestDataWithTime(
-      String key, String model, Duration time) {
+      String key, String model, Duration time) async {
     // TODO: implement writeUserRequestDataWithTime
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> hasToken() async {
+    // TODO: implement hasToken
     throw UnimplementedError();
   }
 }
