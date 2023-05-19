2a5dc06454273309f76c7a93ceb6d69fb108d6e4
CoMatu <30466272+CoMatu@users.noreply.github.com>
2023-05-19 16:42:50 +0300

    fixed

---
+++ integration_test/app_test.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:test_farma/main.dart' as app;
+
+void main() {
+  late IntegrationTestWidgetsFlutterBinding binding;
+
+  setUpAll(() async {
+    binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+  });
+
+  tearDownAll(() async {});
+
+  group('end-to-end test', () {
+    testWidgets('Ввод пинкода', (WidgetTester tester) async {
+      app.main();
+
+      // final deviceParameters =
+      //     await AppRunner.initializeFlutterPluginsAndDependencies();
+
+      // await tester.pumpWidget(ThemeSwitcherWidget(
+      //   child: OfficeApp(
+      //     officeRouterDelegate: sl<OfficeRouterDelegate>(),
+      //     officeRouterInformationParser: OfficeRouterInformationParser(),
+      //     fcmToken: deviceParameters.fcmToken,
+      //   ),
+      // ));
+
+      // await tester.pumpAndSettle();
+
+      // await addDelay(1000);
+
+      // for (var i = 0; i < 4; i++) {
+      //   await addDelay(700);
+      //   await tester.tap(find.byKey(const ValueKey('pin-button-1')));
+      // }
+
+      // await addDelay(7000);
+
+      await tester.pumpAndSettle();
+
+      // expect(
+      //   find.byKey(const ValueKey('bottomNavigationBar')),
+      //   findsOneWidget,
+      // );
+
+      await addDelay(1000);
+
+      takeScreenshot(tester, binding, name: 'name');
+
+      await addDelay(1000);
+    });
+  });
+}
+
+Future<void> addDelay(int ms) async {
+  await Future<void>.delayed(Duration(milliseconds: ms));
+}
+
+Future<void> takeScreenshot(tester, binding, {required String name}) async {
+  if (kIsWeb) {
+    await binding.takeScreenshot(name);
+    return;
+  } else {
+    await binding.convertFlutterSurfaceToImage();
+    await tester.pumpAndSettle();
+  }
+  await binding.takeScreenshot(name);
+}
